61ea4d43a9b4a6d2c5779ae679ea27c18886b036
tungchan2004 <tunglay12310@gmail.com>
2024-11-15 22:50:21 +0700

    update index

--- bt/index.js
+++ bt/index.js
@@ -1,6 +1,6 @@
+import mongoose from 'mongoose';
 import express from "express";
 import rootRouter from "./routes/root.js";
-import { connectDB } from "./config/connectDB.js";
 import userRouter from "./routes/user.js";
 import travelRoutes from './routes/travelRoutes.js';
 import bodyParser from "body-parser";
@@ -8,11 +8,24 @@ import methodOverride from "method-override";
 import adminRoutes from './routes/adminRoutes.js';
 import loginadminRoutes from './routes/loginadminRoutes.js';
 
-connectDB();
+// Kết nối MongoDB
+export const connectDB = async () => {
+  try {
+    await mongoose.connect('mongodb+srv://tunglatoi2004:tunglatoi2004@cluster0.4mxgk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log('Connected to MongoDB Atlas');
+  } catch (err) {
+    console.error('Error connecting to MongoDB:', err);
+    process.exit(1); // Dừng ứng dụng nếu không thể kết nối
+  }
+};
+
+connectDB();  // Gọi hàm kết nối MongoDB trước khi khởi động ứng dụng
+
 const app = express();
 const port = 3000;
-
-
 
 app.use(methodOverride('_method'));
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -27,10 +40,7 @@ app.use(
 );
 app.use(bodyParser.json()); 
 
-
-
-
-// Set view engine and static files
+// Set view engine và static files
 app.set("view engine", "ejs");
 app.set("views", "./bt/views");
 app.use(express.static("./bt/public"));
@@ -39,9 +49,7 @@ app.use(methodOverride('_method'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
-
-
+// Các route
 app.use("/", loginadminRoutes);
 app.use("/", rootRouter);
 app.use('/travel', travelRoutes);
